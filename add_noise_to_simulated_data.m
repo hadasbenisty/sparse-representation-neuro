5957a38316ca52cd4e304adb8a62c6785035a42e
hadasbenisty <hadas.benisty@gmail.com>
2023-06-06 11:16:10 +0300

    update matlab code

--- add_noise_to_simulated_data.m
+++ add_noise_to_simulated_data.m
@@ -6,16 +6,12 @@ newSNR = -10:5:30;
 for fi = 3:length(folders)
     cleanfile = dir(fullfile(folders(fi).folder, folders(fi).name, ['*' num2str(cleanSNR) '*.mat']));
     load(fullfile(cleanfile.folder, cleanfile.name), 'y');
-    sz = size(y);
-    y = double(squeeze(y))';
-    y = y(:);
-    sig_s = std(y);
+    y = double(y);
+    sig_s = std(y(:));
     
     sig_n = sig_s*10.^(-newSNR/20);
     for snr_i = 1:length(newSNR)
-        y_noisy = sig_n(snr_i)*randn(size(y)) + y;
-        y_noisy = y_noisy';
-        y_noisy = reshape(y_noisy, sz);
+        y_noisy = single(sig_n(snr_i)*randn(size(y)) + y);
         save(fullfile(cleanfile.folder, [folders(fi).name, '_SNR_' num2str(newSNR(snr_i)) '.mat']), 'y_noisy');
     end
 end
